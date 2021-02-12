02c93bd7aed1a68fe2faa523c9517d3669b1723e
Emil <emilfigzal@gmail.com>
2021-02-12 21:10:14 +0100

    log4j2 configuration

--- src/main/java/pl/emil7f/Runner.java
+++ src/main/java/pl/emil7f/Runner.java
@@ -1,5 +1,8 @@
 package pl.emil7f;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,17 +11,20 @@ public class Runner {
 
      /**
      * 1. Po dodaniu zależności hibernate-core Tworzymy Entity manager, jeśli już go mamy możemy sobie stworzyć sesje hibernate
-     * Tworzymy entityManagera i możemy zacząć wykonywać jakieś operacje
+     * Tworzymy entityManagera i możemy zacząć wykonywać jakieś operacje  (odpowiednik SessionFactory)
      * Jeśli chcemy stworzyć jakieś transakcje to poprzez getTransaction.begin() rozpoczynamy sesje
      * Na koniec trzeba zrobić commit i zamknąć entityManager.close();
      * W ten sposób przygotowana aplikacja powinna się łączyć z bazą danych
      */
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(Runner.class);
 
     public static void main(String[] args) {
 
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
+
+        logger.info("Logger test");
 
         em.getTransaction().commit();
         em.close();
