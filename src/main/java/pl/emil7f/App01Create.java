55f4994fdb369a3c97babb181b56382189a45c9c
Emil <emilfigzal@gmail.com>
2021-02-12 22:36:38 +0100

    Creation entity

---
+++ src/main/java/pl/emil7f/App01Create.java
@@ -0,0 +1,47 @@
+package pl.emil7f;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Product;
+import pl.emil7f.entity.ProductType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 2. Tworzymy encje, plik schema.sql, a w MySQL Workbench wklejamy skrypt ze schema.sql
+ * Zapis encji, tworzymy produkt. Żeby go zapisać używamy metody persist(product)
+ */
+public class App01Create {
+
+
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(pl.emil7f.Runner.class);
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Product product = new Product();
+        product.setName("Rower 01");
+        product.setDescription("Opis produktu 01");
+        product.setCreated(LocalDateTime.now());
+        product.setUpdated(LocalDateTime.now());
+        product.setPrice(new BigDecimal("379.99"));
+        product.setProductType(ProductType.REAL);
+
+        em.persist(product);
+        logger.info(product);
+
+        em.getTransaction().commit();
+        em.close();
+
+
+    }
+
+
+}
