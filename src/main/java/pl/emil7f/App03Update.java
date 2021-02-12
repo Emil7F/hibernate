79e9633c4b7c4617e4626cffe1bb584bc655772d
Emil <emilfigzal@gmail.com>
2021-02-12 22:53:58 +0100

    update entity

---
+++ src/main/java/pl/emil7f/App03Update.java
@@ -0,0 +1,37 @@
+package pl.emil7f;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * 3. Edycja encji
+ */
+public class App03Update {
+
+
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Product product = em.find(Product.class, 1L);
+        product.setName("Rower 01 extended");
+        Product merged = em.merge(product);
+        logger.info(merged);
+
+        em.getTransaction().commit();
+        em.close();
+
+
+    }
+
+
+}
