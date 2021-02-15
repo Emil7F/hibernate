b8a06c4364f46e7e0db741ccc2dfb2ca7bdfb492
Emil <emilfigzal@gmail.com>
2021-02-15 17:47:39 +0100

    OneToOne bidirecional example

---
+++ src/main/java/pl/emil7f/App09OneToOneBidirectional.java
@@ -0,0 +1,39 @@
+package pl.emil7f;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Category;
+import pl.emil7f.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * 9. OneToOne Bidirecional
+ * W tym przypadku stosując relacje OneToOne, symulujemy sytuacje gdzie jeden produkt ma jedną kategorie..
+ * W rzeczywistości jest inaczej, ale to jest na potrzeby przykładu
+ */
+public class App09OneToOneBidirectional {
+
+
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Category category = em.find(Category.class, 1L);
+        logger.info(category);
+        logger.info(category.getProduct());
+
+        em.getTransaction().commit();
+        em.close();
+
+
+    }
+
+
+}
