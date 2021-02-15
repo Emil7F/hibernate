7121d3af76bb2b41086a874d935bccce074b9c31
Emil <emilfigzal@gmail.com>
2021-02-15 19:40:02 +0100

    ManyToMany delete entity which is not owner of the relation

--- src/main/java/pl/emil7f/App15DeleteManyToMany.java
+++ src/main/java/pl/emil7f/App15DeleteManyToMany.java
@@ -2,17 +2,22 @@ package pl.emil7f;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Attribute;
 import pl.emil7f.entity.Product;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
 
 /**
  * 15. ManyToMany Delete
- *  Product & Attribute
- *  W poniższy sposób czyścimi powiązania z atrybutami po stronie produktu
- * */
+ * Product & Attribute
+ * Hibernate zawsze usuwa powiązania automatycznie po stronie właściciela relacji, jeżeli usuwamy właściciela!
+ *
+ *  w pętli można zastosować new ArrayList jak dostaniemy błąd ConcurrentModificationException
+ *  Opakowywujemy naszego seta/kolecje w inną liste (żeby nie iterować po liście z której usuwamy)
+ */
 public class App15DeleteManyToMany {
 
 
@@ -24,11 +29,13 @@ public class App15DeleteManyToMany {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Product product = em.find(Product.class, 5L);
-        // em.remove(product); // zostanie usunięty produkt wraz z przypisanymi do niego opiniami (opinie usuwają się kaskadowo)
-        // także zostaną usunięte powiązania do atrybutów ale wszyskie atrybuty zostaną zachowane
+        Attribute attribute = em.find(Attribute.class, 1L);
+        for (Product product : new ArrayList<>(attribute.getProducts())) {
 
-        product.getAttributes().clear();
+            attribute.removeProduct(product);
+        }
+
+        em.remove(attribute);
 
 
         em.getTransaction().commit();
