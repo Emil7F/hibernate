496dae077b5f66b72fa4b950cebb26ae55310cf2
Emil <emilfigzal@gmail.com>
2021-02-18 20:38:42 +0100

    basic query with left  join and fetch

--- src/main/java/pl/emil7f/module5/App18Join.java
+++ src/main/java/pl/emil7f/module5/App18Join.java
@@ -11,16 +11,20 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
- *              select   *   from    product p
- *              inner join    category c     on     p.category_id=c.id
- *              where     c.id=1;
+ * select   *   from    product p
+ * inner join    category c     on     p.category_id=c.id
+ * where     c.id=1;
+ * <p>
+ * Takie query zadziała, ale po przeanalizowaniu logów można zobaczyć że hibernate wykonuje dwa zapytania do bazy danych
+ * pomimo tego że zastosowalismy inner join'a
+ * dzieje się tak ze wzgledu na fakt ze hibernate pobiera powiązanie tabele leniwie
+ * <p>
+ * Rozwiązaniem jest wstawienie słowa kluczowego FETCH - które sprawi że wszystko pobierze się jednym zapytaniem
  *
- *     Takie query zadziała, ale po przeanalizowaniu logów można zobaczyć że hibernate wykonuje dwa zapytania do bazy danych
- *     pomimo tego że zastosowalismy inner join'a
- *     dzieje się tak ze wzgledu na fakt ze hibernate pobiera powiązanie tabele leniwie
+ * Konstrukcja z left joinem
  *
- *     Rozwiązaniem jest wstawienie słowa kluczowego FETCH - które sprawi że wszystko pobierze się jednym zapytaniem
- *
+ *  !! join fetch uzywamy do pobierania danych z powiązanych tabel
+ *  a samego joina tylko do filtrowania po powiązaniych tabelach bo nie pobiera on danych z tamtych tabel w tym samym zapytaniu
  */
 
 public class App18Join {
@@ -31,11 +35,13 @@ public class App18Join {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        TypedQuery<Product> query = em.createQuery("select p from Product p " +
-                "inner join fetch p.category c " +
-                "where c.id=:id", Product.class);
+        TypedQuery<Product> query = em.createQuery(
+                "select p from Product p " +
+                        "left join fetch p.category c ",
+                Product.class);
 
-        query.setParameter("id", 1L);
+        //        query.setParameter("id", 1L);
+
         List<Product> resultList = query.getResultList();
 
         for (Product product : resultList) {
