01ede37f84cd2ca349fb532ecdba09c323e950af
Emil <emilfigzal@gmail.com>
2021-02-18 19:34:02 +0100

    mapping query response to dto object

--- src/main/java/pl/emil7f/module5/App17JpqlAResponseMapping.java
+++ src/main/java/pl/emil7f/module5/App17JpqlAResponseMapping.java
@@ -3,12 +3,15 @@ package pl.emil7f.module5;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.ProductInCategoryCounterDto;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App17JpqlAResponseMapping {
 
@@ -25,8 +28,14 @@ public class App17JpqlAResponseMapping {
         );
 
         List<Object[]> resultList = query.getResultList();
-        for (Object[] array : resultList) {
-            logger.info(array[0] + " , " + array[1]);
+
+        List<ProductInCategoryCounterDto> result = resultList.stream()
+                .map(objects -> new ProductInCategoryCounterDto((Long) objects[0], (Long) objects[1]))
+                .collect(Collectors.toList());
+
+        for (ProductInCategoryCounterDto dto : result) {
+            logger.info(dto.getCategoryId());
+            logger.info(dto.getProductInCategoryCounter());
         }
 
 
