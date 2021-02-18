35baa56493cf02af4498fb480aa3e8aa91162dd5
Emil <emilfigzal@gmail.com>
2021-02-18 18:16:59 +0100

    query with aggregate function

--- src/main/java/pl/module5/App17Jpql.java
+++ src/main/java/pl/module5/App17Jpql.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 /**
  * Proste zapytanie JPQL
- * pobieranie produktu z parametrem z zabezpieczeniem w NoResultException
- * za pomocą metody getResultStream i obsługą Optionala
  */
 
 public class App17Jpql {
@@ -23,25 +21,15 @@ public class App17Jpql {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        TypedQuery<Product> query = em.createQuery(
-                "select p from Product p where p.id=:id",
-                Product.class
+        Query query = em.createQuery(
+                "select avg (p.price) from Product p"
         );
 
-        query.setParameter("id", 10000L);
+        Double singleResult = (Double) query.getSingleResult();
+        logger.info(singleResult);
 
-
-        try {
-            Product singleResult = query.getResultStream()
-                    .findFirst()
-                    .orElseThrow(() -> new NoResultException());
-            logger.info(singleResult);
-            em.getTransaction().commit();
-        } catch (NoResultException e) {
-            logger.error("Brak wyników........", e);
-        } finally {
-            em.close();
-        }
+        em.getTransaction().commit();
+        em.close();
 
 
     }
