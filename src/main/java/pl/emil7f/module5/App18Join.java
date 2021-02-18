dbaf7cb120cb2278e8d5e83b750d0661d5c7d7ed
Emil <emilfigzal@gmail.com>
2021-02-18 20:33:11 +0100

    basic query with inner join

---
+++ src/main/java/pl/emil7f/module5/App18Join.java
@@ -0,0 +1,47 @@
+package pl.emil7f.module5;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ *              select   *   from    product p
+ *              inner join    category c     on     p.category_id=c.id
+ *              where     c.id=1;
+ *
+ *     Takie query zadziała, ale po przeanalizowaniu logów można zobaczyć że hibernate wykonuje dwa zapytania do bazy danych
+ *     pomimo tego że zastosowalismy inner join'a
+ *     dzieje się tak ze wzgledu na fakt ze hibernate pobiera powiązanie tabele leniwie
+ */
+
+public class App18Join {
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(App18Join.class);
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Product> query = em.createQuery("select p from Product p " +
+                "inner join p.category c " +
+                "where c.id=:id", Product.class);
+
+        query.setParameter("id", 1L);
+        List<Product> resultList = query.getResultList();
+
+        for (Product product : resultList) {
+            logger.info(product);
+            logger.info(product.getCategory());
+        }
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
