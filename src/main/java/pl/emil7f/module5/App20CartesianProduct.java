12d28c30a76cc0d27b05f7d04bdc56921e18996b
Emil <emilfigzal@gmail.com>
2021-02-20 14:17:43 +0100

    cartesian product example, multiple join

---
+++ src/main/java/pl/emil7f/module5/App20CartesianProduct.java
@@ -0,0 +1,46 @@
+package pl.emil7f.module5;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ * Produkt kartezjański to produkt który powstaje w wyniku mnożenia dwóch zbiorów
+ * Jeśli pobieramy dane z bazy danych z pomocą join'a to dane pobierane są w taki sposób
+ *
+ * Wielkość listy wynosi: 125
+ * Jest 5 produktów, które mają po 5 atrybutów i kazdy produkt ma 5 opinii
+ */
+public class App20CartesianProduct {
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(App20CartesianProduct.class);
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Product> query = em.createQuery(
+                "select p from Product p " +
+                        " left join fetch p.attributes " +
+                        " left join  fetch p.reviews",
+                Product.class);
+
+        List<Product> resultList = query.getResultList();
+        logger.info("Size: " + resultList.size());
+        for (Product product : resultList) {
+            logger.info(product);
+            logger.info(product.getAttributes());
+            logger.info(product.getReviews());
+        }
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
