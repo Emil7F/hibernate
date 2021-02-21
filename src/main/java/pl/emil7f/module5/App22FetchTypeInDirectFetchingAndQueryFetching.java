02662ed07167704f8b1a4bf1c70aa7c5e51233d5
Emil <emilfigzal@gmail.com>
2021-02-21 20:31:53 +0100

    FetchType, find() Eager OneToOne"

--- src/main/java/pl/emil7f/module5/App22FetchTypeInDirectFetchingAndQueryFetching.java
+++ src/main/java/pl/emil7f/module5/App22FetchTypeInDirectFetchingAndQueryFetching.java
@@ -10,7 +10,8 @@ import javax.persistence.Persistence;
 
 /**
  * Jak mogą się zmieniać zapytania gdy korzystamy z róznych parametrów zapytania FetchType - find()
- * Category pobiera się leniwie
+ * Category pobiera się gorliwie
+ * Hibernate generuje left outer join, czyli w pojedyńczym select'cie probuje on optymalizować pobieranie powiązanych relacji poprzez join'a
  *
  Hibernate:
  select
@@ -21,25 +22,20 @@ import javax.persistence.Persistence;
  product0_.name as name4_5_0_,
  product0_.price as price5_5_0_,
  product0_.type as type6_5_0_,
- product0_.updated as updated7_5_0_
+ product0_.updated as updated7_5_0_,
+ category1_.id as id1_1_1_,
+ category1_.description as descript2_1_1_,
+ category1_.name as name3_1_1_
  from
  Product product0_
+ left outer join
+ Category category1_
+ on product0_.category_id=category1_.id
  where
  product0_.id=?
- 2021-02-21 20:27:52.589 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Product{id=1, name='Rower 01', description='To jest opis produktu', created=2020-07-22T15:29:39, updated=2020-07-22T15:29:39, price=19.99, productType=REAL}
+ 2021-02-21 20:29:10.364 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Product{id=1, name='Rower 01', description='To jest opis produktu', created=2020-07-22T15:29:39, updated=2020-07-22T15:29:39, price=19.99, productType=REAL}
+ 2021-02-21 20:29:10.364 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Category{id=1, name='Kategoria 1', description='Opis 1'}
 
- Hibernate:
- select
- category0_.id as id1_1_0_,
- category0_.description as descript2_1_0_,
- category0_.name as name3_1_0_
- from
- Category category0_
- where
- category0_.id=?
- 2021-02-21 20:27:52.594 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Category{id=1, name='Kategoria 1', description='Opis 1'}
-
- Process finished with exit code 0
 
  * */
 
