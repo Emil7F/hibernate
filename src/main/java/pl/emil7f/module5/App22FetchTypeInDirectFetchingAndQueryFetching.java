dded954c59a346bea156df0e307555d47b3b1baf
Emil <emilfigzal@gmail.com>
2021-02-21 20:44:01 +0100

    FetchType, JPQL  Eager OneToOne, ManyToOne Eager

--- src/main/java/pl/emil7f/module5/App22FetchTypeInDirectFetchingAndQueryFetching.java
+++ src/main/java/pl/emil7f/module5/App22FetchTypeInDirectFetchingAndQueryFetching.java
@@ -9,48 +9,56 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /**
- * Jak mogą się zmieniać zapytania gdy korzystamy z róznych parametrów zapytania FetchType - find()
+ * Jak mogą się zmieniać zapytania gdy korzystamy z róznych parametrów zapytania FetchType - JPQL
  * Category pobiera się gorliwie
  * Reviews pobiera się gorliwie
- * Hibernate generuje left outer join przy zapytaniu do Category oraz dla Reviews
- * czyli w pojedyńczym select'cie probuje on optymalizować pobieranie powiązanych relacji poprzez join'a
+ * Są trzy zapytania które pobierają się natychmiast, nawet jeśli nie będziemy potrzebować tych danych to one się nam pobiorą ( w dod. zapytaniach)
+ *
  *
  Hibernate:
  select
- product0_.id as id1_5_0_,
- product0_.category_id as category8_5_0_,
- product0_.created as created2_5_0_,
- product0_.description as descript3_5_0_,
- product0_.name as name4_5_0_,
- product0_.price as price5_5_0_,
- product0_.type as type6_5_0_,
- product0_.updated as updated7_5_0_,
- category1_.id as id1_1_1_,
- category1_.description as descript2_1_1_,
- category1_.name as name3_1_1_,
- reviews2_.product_id as product_4_7_2_,
- reviews2_.id as id1_7_2_,
- reviews2_.id as id1_7_3_,
- reviews2_.content as content2_7_3_,
- reviews2_.product_id as product_4_7_3_,
- reviews2_.rating as rating3_7_3_
+ product0_.id as id1_5_,
+ product0_.category_id as category8_5_,
+ product0_.created as created2_5_,
+ product0_.description as descript3_5_,
+ product0_.name as name4_5_,
+ product0_.price as price5_5_,
+ product0_.type as type6_5_,
+ product0_.updated as updated7_5_
  from
  Product product0_
- left outer join
- Category category1_
- on product0_.category_id=category1_.id
- left outer join
- Review reviews2_
- on product0_.id=reviews2_.product_id
  where
  product0_.id=?
- 2021-02-21 20:37:22.250 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Product{id=1, name='Rower 01', description='To jest opis produktu', created=2020-07-22T15:29:39, updated=2020-07-22T15:29:39, price=19.99, productType=REAL}
- 2021-02-21 20:37:22.251 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Category{id=1, name='Kategoria 1', description='Opis 1'}
- 2021-02-21 20:37:22.251 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - [Review{id=1, content='Treść opinii 1', rating=5}, Review{id=4, content='Treść opinii 4', rating=5}, Review{id=2, content='Treść opinii 2', rating=5}, Review{id=3, content='Treść opinii 3', rating=5}, Review{id=5, content='Treść opinii 5', rating=5}]
+
+ Hibernate:
+ select
+ category0_.id as id1_1_0_,
+ category0_.description as descript2_1_0_,
+ category0_.name as name3_1_0_
+ from
+ Category category0_
+ where
+ category0_.id=?
+
+ Hibernate:
+ select
+ reviews0_.product_id as product_4_7_0_,
+ reviews0_.id as id1_7_0_,
+ reviews0_.id as id1_7_1_,
+ reviews0_.content as content2_7_1_,
+ reviews0_.product_id as product_4_7_1_,
+ reviews0_.rating as rating3_7_1_
+ from
+ Review reviews0_
+ where
+ reviews0_.product_id=?
+ 2021-02-21 20:41:04.236 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Product{id=1, name='Rower 01', description='To jest opis produktu', created=2020-07-22T15:29:39, updated=2020-07-22T15:29:39, price=19.99, productType=REAL}
+ 2021-02-21 20:41:04.236 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - Category{id=1, name='Kategoria 1', description='Opis 1'}
+ 2021-02-21 20:41:04.236 INFO  [main] App22FetchTypeInDirectFetchingAndQueryFetching - [Review{id=1, content='Treść opinii 1', rating=5}, Review{id=2, content='Treść opinii 2', rating=5}, Review{id=4, content='Treść opinii 4', rating=5}, Review{id=5, content='Treść opinii 5', rating=5}, Review{id=3, content='Treść opinii 3', rating=5}]
 
  Process finished with exit code 0
 
- * */
+ */
 
 public class App22FetchTypeInDirectFetchingAndQueryFetching {
 
@@ -62,7 +70,12 @@ public class App22FetchTypeInDirectFetchingAndQueryFetching {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Product product = em.find(Product.class, 1L);
+        Product product = em.createQuery(
+                "select p from Product p " +
+                        "where p.id=:id",
+                Product.class)
+                .setParameter("id", 1L)
+                .getSingleResult();
 
         logger.info(product);
         logger.info(product.getCategory());
