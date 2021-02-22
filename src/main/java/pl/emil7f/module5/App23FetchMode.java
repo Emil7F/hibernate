7b354d82974997c7720072223d6177c02013d3f1
Emil <emilfigzal@gmail.com>
2021-02-22 18:51:54 +0100

    FetchMod

---
+++ src/main/java/pl/emil7f/module5/App23FetchMode.java
@@ -0,0 +1,57 @@
+package pl.emil7f.module5;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *   Pobieramy Order za pomocą find, a OrderRow dociąga się leniwie (FetchType.LAZY)
+ *
+ Hibernate:
+ select
+ order0_.id as id1_3_0_,
+ order0_.created as created2_3_0_,
+ order0_.total as total3_3_0_
+ from
+ `
+ order` order0_ where
+ order0_.id=?
+ 2021-02-22 18:40:14.821 INFO  [main] App23FetchMode - Order{id=1, created=2020-10-10T14:00, total=59.97}
+
+ Hibernate:
+ select
+ orderrows0_.order_id as order_id4_4_0_,
+ orderrows0_.id as id1_4_0_,
+ orderrows0_.id as id1_4_1_,
+ orderrows0_.price as price2_4_1_,
+ orderrows0_.product_id as product_3_4_1_
+ from
+ order_row orderrows0_
+ where
+ orderrows0_.order_id=?
+ 2021-02-22 18:40:14.841 INFO  [main] App23FetchMode - [OrderRow{id=2, price=19.99}, OrderRow{id=3, price=19.99}, OrderRow{id=1, price=19.99}]
+
+ *
+ */
+
+public class App23FetchMode {
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(App23FetchMode.class);
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Order order = em.find(Order.class, 1L);
+        logger.info(order);
+        logger.info(order.getOrderRows());
+
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
