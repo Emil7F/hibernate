3bbade10e9d9f53d574590aaee758545025d34a7
Emil <emilfigzal@gmail.com>
2021-02-26 19:09:01 +0100

    n+1 example

---
+++ src/main/java/pl/emil7f/module5/App26NplusOne.java
@@ -0,0 +1,39 @@
+package pl.emil7f.module5;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+
+/*
+Problem n+1
+ */
+public class App26NplusOne {
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+    private static Logger logger = LogManager.getLogger(App26NplusOne.class);
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        List<Order> products = em.createQuery(
+                "select o from Order o",
+                Order.class)
+                .getResultList();
+
+        logger.info("Ilość rekordów: " + products.size());
+
+        for (Order order : products) {
+            logger.info(order);
+            logger.info(order.getOrderRows());
+        }
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
