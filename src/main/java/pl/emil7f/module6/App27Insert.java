db55abb3e42e631ab1f62d1a1e9339a333d5a684
Emil <emilfigzal@gmail.com>
2021-02-26 20:30:15 +0100

    flush and clear session memory

--- src/main/java/pl/emil7f/module6/App27Insert.java
+++ src/main/java/pl/emil7f/module6/App27Insert.java
@@ -14,6 +14,10 @@ Hibernate wyłącza mechanizm batchowania dla encji w których typ batchowania t
 Najwygodniej to robić na db która wspiera sekwencje -> GenerationType.SEQUENCE, np w PostgreSQL
 wtedy ustawiamy odpowiednio properties batch_size.
 Nie trzeba wtedy bawić się w budowanie dodatkowych encji oraz w zapytania po ostatni ID żeby dodać nowy wiersz w tabeli
+
+Hibernate cache'uje sobie encje w sesji, każda encja tworzona w pętli jest w cache'u sesji, do zamknięcia sesji
+Jesli będziemy dodawać setki rekordów to możemy zapchać pamięć
+Można temu zapobiec poprze czyszczenie sesji z obietków batchReview co jakiś czas
  */
 
 public class App27Insert {
@@ -28,6 +32,10 @@ public class App27Insert {
         Long lastId = em.createQuery("select max(r.id) from BatchReview r ", Long.class).getSingleResult();
 
         for (long i = 1; i <= 25; i++) {
+            if(i % 5 == 0){
+                em.flush();    // zapis danych wszystkich niezapisanych encji
+                em.clear();   // co 5 elementów nastąpi przeładowanie i czyszczenie sesji
+            }
             em.persist(new BatchReview(lastId + i, "Treść " + i, (int) (i + 5 % 2), 1L));
 
         }
