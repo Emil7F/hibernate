0af5b0bc3aac638225949b5890ab807582bbbcc9
Emil <emilfigzal@gmail.com>
2021-03-02 20:18:27 +0100

    criteria api filtering And Or

---
+++ src/main/java/pl/emil7f/module7/App34CriteriaFiltering.java
@@ -0,0 +1,64 @@
+package pl.emil7f.module7;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.emil7f.entity.Customer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class App34CriteriaFiltering {
+    private static Logger logger = LogManager.getLogger(App34CriteriaFiltering.class);
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Customer> criteriaQuery = criteriaBuilder.createQuery(Customer.class);
+        Root<Customer> customerRoot = criteriaQuery.from(Customer.class);
+
+        ParameterExpression<Long> id1 = criteriaBuilder.parameter(Long.class);
+        ParameterExpression<Long> id2 = criteriaBuilder.parameter(Long.class);
+        ParameterExpression<BigDecimal> total = criteriaBuilder.parameter(BigDecimal.class);
+
+        Join<Object, Object> orders = (Join<Object, Object>) customerRoot
+                .fetch("orders", JoinType.INNER);
+
+        orders.fetch("orderRows")
+                .fetch("product");
+
+        criteriaQuery.select(customerRoot).distinct(true)
+                .where(
+                        criteriaBuilder.and(
+                                criteriaBuilder.or(
+                                        criteriaBuilder.equal(customerRoot.get("id"), id1),
+                                        criteriaBuilder.equal(customerRoot.get("id"), id2)),
+                                criteriaBuilder.greaterThan(orders.get("total"), total)
+                        )
+                );
+
+        TypedQuery<Customer> query = em.createQuery(criteriaQuery);
+        query.setParameter(id1, 1L);
+        query.setParameter(id2, 2L);
+        query.setParameter(total, new BigDecimal(30.00));
+
+        List<Customer> resultList = query.getResultList();
+        for (Customer result : resultList) {
+            logger.info(result);
+            logger.info(result.getOrders());
+        }
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
